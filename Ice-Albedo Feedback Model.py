e4d85ead223119cd81f08b49d011b209e5dcb7c6
Loise Nyambura Ndiritu <96653220+loisendiritu@users.noreply.github.com>
2021-12-26 13:17:04 +0300

    Add files via upload

---
+++ Ice-Albedo Feedback Model.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Predefined ranges for L & albedo
+LRange = [1150, 1350]
+albedoRange = [0.15, 0.65]
+L, albedo, nIters = LRange[1], albedoRange[0], 100
+
+#Predefined variables obtained from Part I & Spreadsheet Linear Eqn
+albedoM = -0.01
+albedoB = 2.8
+sigma = 5.67E-8
+
+#IInitializing x & y lists for graph plotting
+x, y = [], []
+
+while L > LRange[0]-1:
+    for i in range(1,nIters+1):
+        #Guess and check loop to get temperature T & albedo
+        T = L * (1 - albedo) / (4 * sigma)
+        T = pow(T, 0.25)
+        albedo = T * albedoM + albedoB
+
+        #Limits albdeo to the specified range of 0.15 - 0.65
+        albedo = min(albedo, albedoRange[1])
+        albedo = max(albedo, albedoRange[0])
+
+        x.append(i)
+        y.append(T)
+    x.append(np.nan)
+    y.append(np.nan)
+    L -= 10
+
+#Generates a plot temperatures plotted as a function of iteration number
+plt.xlabel("Iteration Number")
+plt.ylabel("Temperature")
+plt.title("Snowball")
+plt.plot(x, y)
+plt.show()
