a0263befaf97c92821e0908afd7a886f25e8ed6d
Loise Nyambura Ndiritu <96653220+loisendiritu@users.noreply.github.com>
2021-12-31 13:30:27 +0300

    Update Ice-Albedo Feedback Model.py

--- Ice-Albedo Feedback Model.py
+++ Ice-Albedo Feedback Model.py
@@ -1,27 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#Predefined ranges for L & albedo
+# Predefined ranges for L & albedo
 LRange = [1150, 1350]
 albedoRange = [0.15, 0.65]
 L, albedo, nIters = LRange[1], albedoRange[0], 100
 
-#Predefined variables obtained from Part I & Spreadsheet Linear Equation
+# Predefined variables obtained from Part I & Spreadsheet Linear Equation
 albedoM = -0.01
 albedoB = 2.8
 sigma = 5.67E-8
 
-#IInitialize x & y lists for graph plotting
+# IInitialize x & y lists for graph plotting
 x, y = [], []
 
 while L > LRange[0]-1:
     for i in range(1,nIters+1):
-        #Guess and check loop to get temperature T & albedo
+        # Guess and check loop to get temperature T & albedo
         T = L * (1 - albedo) / (4 * sigma)
         T = pow(T, 0.25)
         albedo = T * albedoM + albedoB
 
-        #Limits albdeo to the specified range of 0.15 - 0.65
+        # Limits albdeo to the specified range of 0.15 - 0.65
         albedo = min(albedo, albedoRange[1])
         albedo = max(albedo, albedoRange[0])
 
@@ -31,7 +31,7 @@ while L > LRange[0]-1:
     y.append(np.nan)
     L -= 10
 
-#Generates a plot temperatures plotted as a function of iteration number
+# Generates a plot temperatures plotted as a function of iteration number
 plt.xlabel("Iteration Number")
 plt.ylabel("Temperature")
 plt.title("Snowball")
